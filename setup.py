123858c67b7136fed9ae5e65dea25e4cec0639c8
Jon Swanson <jswan2@uw.edu>
2012-12-18 15:56:33 -0800

    made labgeeks_delphi and egg for buildout deployment

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'labgeeks-delphi',
+    version = '1.0',
+    license = 'Apache',
+    url = 'http://github.com/abztrakt/labgeeks_delphi',
+    description = 'The knowledge base app in the labgeeks suite of student staff management tools.',
+    author = 'Craig Stimmel',
+    packages = find_packages(),
+    install_requires = [
+        'setuptools',
+        'labgeeks-sybil',
+        'South==0.7.3',
+        'Markdown==2.2.0',
+    ],
+)
